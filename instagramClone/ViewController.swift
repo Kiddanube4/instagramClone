02fc72d330f6174d17e5025dc595a0816ff3f9ea
Namık Karabıyık <namik.karabiyik@ddtech.com.tr>
2022-04-06 13:48:02 +0300

    Initial Commit

---
+++ instagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagramClone
+//
+//  Created by Namık Karabıyık on 6.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
